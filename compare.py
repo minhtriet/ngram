9e67a01e80ba1689e05fb3a300718f38346d1b54
minhtriet <minhtriet09@gmail.com>
2017-08-27 13:39:18 +0200

    add metric

--- compare.py
+++ compare.py
@@ -1,13 +1,19 @@
 import sys
 from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
 
-f = open(sys.argv[1], 'r') 
+f = open(sys.argv[1], 'r')
 y_pred = f.read().split('\n')
 f.close()
 
-f = open(sys.argv[2], 'r') 
+f = open(sys.argv[2], 'r')
 y_true = f.read().split('\n')
 f.close()
 
-confusion_matrix(y_true, y_pred)
+cm = confusion_matrix(y_true, y_pred)
+plt.figure()
+plt.ylabel('True label')
+plt.xlabel('Predicted label')
 
+plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+plt.show()
